2cc6b27f60f8042a4fadbe3fa4782a4b418a63bf
Giussepe Delgado <giussepe@latamready.com>
2023-10-31 12:03:11 -0500

    D1214 - MX: Validacion void
    tes de pruebas agregados

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-value = value === true ? "T" : value === false ? "F" : value;
-result["custpage_detailed_rounding"] = value === true ? "T" : value === false ? "F" : value;
 
+
+var arrayTransacciones = "12345 6789 42 99";
+let aux_arrayTransacciones = arrayTransacciones.match(/\b\d*\b/g);
+var arrayTransacciones = [];
+if (aux_arrayTransacciones) {
+    for (i = 0; i < aux_arrayTransacciones.length; i++) {
+        if (aux_arrayTransacciones[i]) arrayTransacciones.push(aux_arrayTransacciones[i]);
+    }
+}
+console.log(arrayTransacciones);
+console.log(arrayTransacciones.length);
