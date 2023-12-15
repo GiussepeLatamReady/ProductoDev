c4de481928c5c7026c81517275561a4c1d4e5469
Giussepe Delgado <giussepe@latamready.com>
2023-12-15 13:19:17 -0500

    C1040 - CL redondeo
    Cambio de exchangerate

--- z_testing/Lib/LMRY_EI_PriceUnit_LBRY_V2.0.js
+++ z_testing/Lib/LMRY_EI_PriceUnit_LBRY_V2.0.js
@@ -11,15 +11,15 @@
  * @NModuleScope Public
  */
 define([
-        'N/format',
-        'N/log',
-        'N/search',
-        'N/runtime',
-        'N/currency',
-        './LMRY_EI_libSendingEmailsLBRY_V2.0',
-        
-    ],
-    function (format, log, search,runtime, currencyApi, ei_library) {
+    'N/format',
+    'N/log',
+    'N/search',
+    'N/runtime',
+    'N/currency',
+    './LMRY_EI_libSendingEmailsLBRY_V2.0',
+
+],
+    function (format, log, search, runtime, currencyApi, ei_library) {
 
         /**
          * Funcion que permite calcular el precio unitario y mnto taotal de una linea de transaccion
@@ -44,14 +44,14 @@ define([
                                 fieldId: "itemunitprice",
                                 line: i
                             }) || 0;
-                        } else{
+                        } else {
                             unitPrice = currentRecord.getSublistValue({
                                 sublistId: "item",
                                 fieldId: "rate",
                                 line: i
                             }) || 0;
                         }
-                        
+
                         var quantity = currentRecord.getSublistValue({
                             sublistId: "item",
                             fieldId: "quantity",
@@ -80,79 +80,82 @@ define([
             }
         }
 
-        function getExchangeRate(currentRCD) {
-            var featureSubsidiary = runtime.isFeatureInEffect({
-                feature: "SUBSIDIARIES"
-              });
-            var localmoneda = localCurrency(currentRCD);
-            if (featureSubsidiary) {
-                var subsimoneda = subsiCurrency(currentRCD);
-            } else {
-                var doc_subsi = currentRCD.getValue('subsidiary');
-                var objCountry = ei_library.getCountryID(doc_subsi);
-                var subsimoneda = objCountry.currency;
-            }
-            var transactionCurrency = currentRCD.getValue('currency');
-            log.error("subsi-localmoneda-subsimoneda", featureSubsidiary + '-' + localmoneda + '-' + subsimoneda)
-            if (subsimoneda == transactionCurrency) {
-                return 1;
-            } else {
-                var bookExchangeRate = '';
-                if (localmoneda != '') {
-                    var trandate = format.parse({
-                        value: currentRCD.getValue('trandate'),
-                        type: format.Type.DATE
-                    });
+        function getExchangeRate(recordObj) {
+            var exchangerate = 1;
+            var exchangerateTransaction = recordObj.getValue({ fieldId: 'exchangerate' })
+            var FEAT_SUBSIDIARY = runtime.isFeatureInEffect({ feature: "SUBSIDIARIES" });
+            var FEAT_MULTIBOOK = runtime.isFeatureInEffect({ feature: "MULTIBOOK" });
+            if (FEAT_SUBSIDIARY && FEAT_MULTIBOOK) {
 
-                    bookExchangeRate = currencyApi.exchangeRate({
-                        date: trandate,
-                        source: currentRCD.getValue('currency'),
-                        target: localmoneda
-                    });
+                var subsidiary = recordObj.getValue('subsidiary');
+                var currencySetup = getCurrencySetupTaxSubsidiary(subsidiary);
+                var currencySubs = search.lookupFields({
+                    type: 'subsidiary',
+                    id: subsidiary,
+                    columns: ['currency']
+                }).currency[0].value;
+
+                var numLines = recordObj.getLineCount({
+                    sublistId: 'accountingbookdetail'
+                });
+
+                if (currencySetup != "" && currencySetup != currencySubs) {
+                    if (numLines) {
+                        for (var i = 0; i < numLines; i++) {
+                            var currencyMB = recordObj.getSublistValue({
+                                sublistId: 'accountingbookdetail',
+                                fieldId: 'currency',
+                                line: i
+                            });
+
+                            if (currencyMB == currencySetup) {
+                                exchangerate = recordObj.getSublistValue({
+                                    sublistId: 'accountingbookdetail',
+                                    fieldId: 'exchangerate',
+                                    line: i
+                                });
+                                break;
+                            }
+                        }
+                    }
+                } else {
+                    exchangerate = exchangerateTransaction
                 }
-                return bookExchangeRate;
-            }
-        }
-
-        function localCurrency(currentRCD) {
-            var localcu = search.create({
-                type: "customrecord_lmry_setup_tax_subsidiary",
-                columns: [
-                    "custrecord_lmry_setuptax_currency"
-                ],
-                filters: [
-                    ["custrecord_lmry_setuptax_subsidiary", "anyof", currentRCD.getValue('subsidiary')], "AND",
-                    ["isinactive", "is", "F"],
-                ]
-            });
-
-            localcu = localcu.run().getRange(0, 1);
-
-            if (localcu.length > 0) {
-                return localcu[0].getValue('custrecord_lmry_setuptax_currency');
             } else {
-                return "";
+                exchangerate = exchangerateTransaction
             }
+
+            return exchangerate;
         }
 
-        function subsiCurrency(currentRCD) {
-            var subsicu = search.create({
-                type: "subsidiary",
+
+        function getCurrencySetupTaxSubsidiary(subsidiary) {
+            try {
+      
+              var filters = [['isinactive', 'is', 'F'], 'AND'];
+              if (FEAT_SUBSIDIARY) {
+                filters.push(['custrecord_lmry_setuptax_subsidiary', 'anyof', subsidiary]);
+              }
+      
+              var searchSetupTaxSub = search.create({
+                type: 'customrecord_lmry_setup_tax_subsidiary',
+                filters: filters,
                 columns: [
-                    "currency"
-                ],
-                filters: [
-                    ["internalid", "anyof", currentRCD.getValue('subsidiary')], "AND",
-                    ["isinactive", "is", "F"],
+                  'custrecord_lmry_setuptax_currency'
                 ]
-            });
-
-            subsicu = subsicu.run().getRange(0, 1);
-
-            if (subsicu.length > 0) {
-                return subsicu[0].getValue('currency');
+              });
+      
+              var resultSetupTaxSub = searchSetupTaxSub.run().getRange(0, 1);
+              if (resultSetupTaxSub && resultSetupTaxSub.length > 0) {
+                returm = resultSetupTaxSub[0].getValue('custrecord_lmry_setuptax_currency') || "";
+              }
+      
+              return "";
+            } catch (err) {
+              log.error('[setupTaxSubsidiary]', err);
             }
-        }
+          }
+
         return {
             setCLAmountLines: setCLAmountLines
         };
