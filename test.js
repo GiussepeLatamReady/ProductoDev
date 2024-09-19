b4a03c94b5e9ac578063309ea920fd5190cc6468
Giussepe Delgado <giussepe@latamready.com>
2024-09-19 17:15:27 -0500

    D1652 -  ALL Formulario Address
    Esta la funcion que posiblemente sea recursiva

--- test.js
+++ test.js
@@ -1,19 +1,84 @@
-function validateLocalized(newRecord) {
-    if (!runtime.isFeatureInEffect({ feature: 'SUBSIDIARIES' })) return false;
+function isActiveSubsidiary(recordObj){
+    var subsi = recordObj.getValue({
+      fieldId: "subsidiary",
+    });
+    log.error("subsi",subsi)
+    var includeChildren= recordObj.getValue({
+      fieldId: "includechildren",
+    });
+    log.error("includeChildren",includeChildren)
+    /**
+     * afdasdfasdf
+     */
+    var subsis = search.create({
+      type: "subsidiary",
+      columns: ["internalid", "country", "isinactive", "parent"]
+    }).run().getRange(0, 1000);
+    
+    var subsisParent = {}; 
+    if (subsis && subsis.length) {
+      for (var i = 0; i < subsis.length; i++) {
+        var parent = subsis[i].getValue("parent");
+        var country = subsis[i].getValue("country");
+        var id = subsis[i].getValue("internalid");
+        var inactive = subsis[i].getValue("isinactive");
+        
+        subsisParent[id] = {p:parent, c:country, i:inactive};
 
-    const { href, searchParams } = new URL(document.referrer);
-    const subsidiaryID = searchParams.get(href.includes("subsidiarytype") ? "id" : "subsidiary");
-
-    if (subsidiaryID && !isLocalized(subsidiaryID)) {
-        hideLatamFields();
-    } else if (href.includes("entity")) {
-        const entityID = searchParams.get("id");
-        const subsidiaryID = search.lookupFields({
-            type: search.Type.ENTITY,
-            id: entityID,
-            columns: ['subsidiary']
-        }).subsidiary?.[0]?.value;
-
-        if (subsidiaryID && !isLocalized(subsidiaryID)) hideLatamFields();
+      }
     }
-}
+
+    var aux = subsi;
+    var aux2 = subsi;
+    var condition = false;
+    while (!condition || !aux.length) {
+      aux2 = [];
+      for (var id in subsisParent) {
+        if (aux.indexOf(id) > -1) {
+          if (subsisParent[id].c == "MX" && subsisParent[id].i) {
+            condition = true;
+            break;
+          }
+        }
+        if (aux.indexOf(subsisParent[id].p) > -1){
+          aux2.push(id);
+        }
+      }
+      aux = aux2;
+    }
+    
+    /**
+     * 
+     */
+    var subsidiaryInfo = search.lookupFields({
+      type: search.Type.SUBSIDIARY,
+      id: subsi,
+      columns: ['isinactive','country']
+    });
+    var isinactive = subsidiaryInfo.isinactive;
+    var country = subsidiaryInfo.country[0].value;
+
+    log.error("isinactive",isinactive)
+    log.error("country",country)
+    if (country == "MX"){
+      return isinactive !== "T" && isinactive !== true;
+    }else if(includeChildren === "T"|| includeChildren === true){
+      var searchOW = search.create({
+        type: 'subsidiary',
+        columns: [
+            'country',
+            'isinactive'
+        ],
+        filters: [
+            ['isinactive', 'is', 'F'],
+            "AND",
+            ['parent', 'anyof', subsi],
+            "AND",
+            ['country', 'anyof', "MX"],
+        ]
+      }); 
+      return searchOW.runPaged().count;
+    }
+    
+      
+  }
