43dbecc8a3ef63287be6afecc75f7a18e61f3e14
Giussepe Delgado <giussepe@latamready.com>
2025-01-10 08:23:04 -0500

    D1738 - MX - EI Void Payment - Reversal Journal Traslado de IVA
    Anotaciones

--- Z_TESTING/LMRY_AnulacionInvoice_LBRY_V2.0.js
+++ Z_TESTING/LMRY_AnulacionInvoice_LBRY_V2.0.js
@@ -1317,7 +1317,7 @@ define(['N/record', 'N/runtime', 'N/log', 'N/search', 'N/format', 'N/transaction
                 var custPymtAccountDetail = query.runSuiteQL({
                     query: "SELECT BUILTIN.DF(TransactionAccountingLine.Account) AS Account, TransactionAccountingLine.Account, TransactionAccountingLine.Debit, TransactionAccountingLine.Credit, TransactionAccountingLine.Posting, TransactionLine.Memo, TransactionLine.id, TransactionLine.Transaction, accountingbook.isprimary FROM accountingbook, TransactionLine, TransactionAccountingLine WHERE TransactionLine.Transaction = TransactionAccountingLine.Transaction AND TransactionAccountingLine.accountingbook = accountingbook.id AND accountingbook.isprimary = 'T' AND TransactionLine.id = '0' AND TransactionAccountingLine.Transaction = '" + recordId + "'  AND (TransactionAccountingLine.Debit IS NOT NULL OR TransactionAccountingLine.Credit IS NOT NULL) ORDER BY TransactionLine.ID" }).results;
 
-
+                
                 // log.error("data custPymtAccountDetail", [custPymtAccountDetail[0], custPymtAccountDetail[1]]);
                 var accountDebit = custPymtAccountDetail.filter(function (cuenta) { return cuenta.values[2] == null })[0].values[1];
                 var accountCredit = custPymtAccountDetail.filter(function (cuenta) { return cuenta.values[3] == null })[0].values[1];
