10468609565a6ea3ce3864e95313b4c1de0a418d
Giussepe Delgado <giussepe@latamready.com>
2025-02-05 12:19:02 -0500

    D1738 - MX - EI Void Payment - Reversal Journal Traslado de IVA
    Correcion por el tipo de moneda

--- test.sql
+++ test.sql
@@ -1,25 +1,58 @@
 SELECT
     BUILTIN.DF(TransactionAccountingLine.Account) AS Account,
     TransactionAccountingLine.Account,
-    TransactionAccountingLine.Debit,
-    TransactionAccountingLine.Credit,
+    TransactionAccountingLine.Debit / Transaction.ExchangeRate AS DebitInTransactionCurrency,
+    TransactionAccountingLine.Credit / Transaction.ExchangeRate AS CreditInTransactionCurrency,
+    TransactionAccountingLine.Posting,
+    TransactionLine.Memo,
+    TransactionLine.id,
+    TransactionLine.Transaction
+FROM
+    accountingbook,
+    TransactionLine,
+    TransactionAccountingLine,
+    Transaction
+WHERE
+    TransactionLine.Transaction = TransactionAccountingLine.Transaction
+    AND TransactionAccountingLine.Transaction = Transaction.ID
+    AND accountingbook.isprimary = 'T'
+    AND TransactionAccountingLine.accountingbook = accountingbook.id
+    AND TransactionLine.id = '0'
+    AND TransactionAccountingLine.Transaction = '" + recordId + "'
+    AND (
+        TransactionAccountingLine.Debit IS NOT NULL
+        OR TransactionAccountingLine.Credit IS NOT NULL
+    )
+ORDER BY
+    TransactionLine.ID;
+
+SELECT
+    BUILTIN.DF(TransactionAccountingLine.Account) AS Account,
+    TransactionAccountingLine.Account,
+    TransactionAccountingLine.Debit / Transaction.ExchangeRate AS DebitInTransactionCurrency,
+    TransactionAccountingLine.Credit / Transaction.ExchangeRate AS CreditInTransactionCurrency,
     TransactionAccountingLine.Posting,
     TransactionLine.Memo,
     TransactionLine.id,
     TransactionLine.Transaction,
+    accountingbook.id AS book,
+    Transaction.Currency AS TransactionCurrency,
+    Transaction.ExchangeRate AS ExchangeRate
 FROM
     accountingbook,
     TransactionLine,
-    TransactionAccountingLine
-where
+    TransactionAccountingLine,
+    Transaction
+WHERE
     TransactionLine.Transaction = TransactionAccountingLine.Transaction
-    and TransactionAccountingLine.accountingbook = accountingbook.id
-    and accountingbook.isprimary = 'T'
-    and TransactionLine.id = "0"
-    and TransactionAccountingLine.Transaction = '4286427'
-    and (
+    AND TransactionAccountingLine.Transaction = Transaction.ID
+    AND accountingbook.isprimary = 'T'
+    AND TransactionAccountingLine.accountingbook = accountingbook.id
+    AND TransactionLine.id = '0'
+    AND TransactionAccountingLine.Transaction = '" + recordId + "'
+    AND (
         TransactionAccountingLine.Debit IS NOT NULL
-        or TransactionAccountingLine.Credit IS NOT NULL
+        OR TransactionAccountingLine.Credit IS NOT NULL
     )
 ORDER BY
-    TransactionLine.ID
+    TransactionLine.ID;
