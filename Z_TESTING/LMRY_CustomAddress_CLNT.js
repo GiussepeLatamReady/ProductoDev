98144573e8f9cc65a89acc562087c1286804ab3a
Giussepe Delgado <giussepe@latamready.com>
2025-06-04 14:49:58 -0500

    ALL Formulario Address
    Implementa la funcionalidad para localizar direcciones, permitiendo la personalización de campos y la validación basada en la subsidiaria y el país.
    
    - Agrega un nuevo campo "Latam - City" que se actualiza dinámicamente basado en la provincia seleccionada.
    - Oculta los campos Latam que no corresponden según la configuración de la subsidiaria.
    - Valida y oculta campos SAP específicos para Colombia, como "dropdownstate" y "city".
    - Utiliza sessionStorage para mantener el historial de navegación y determinar la subsidiaria actual.

--- Z_TESTING/LMRY_CustomAddress_CLNT.js
+++ Z_TESTING/LMRY_CustomAddress_CLNT.js
@@ -3,45 +3,26 @@
  * @NModuleScope Public
  * @Name LMRY_CustomAddress_CLNT.js
  */
-define([
-    "N/search", 
-    "N/currentRecord",
-    "N/runtime"
-], function (
-    search, 
-    currentRecord,
-    runtime
-) {
+define(["N/search", "N/currentRecord", "N/runtime"], function (search, currentRecord,runtime) {
     function brCustomCity() {
-        console.log("Addres script")
-        
         // const urlt = new URL(window.location.href);
         const recordNow = currentRecord.get();
-        validateLocalized(recordNow);
+        const country = recordNow.getValue("country")
         // if (urlt.searchParams.get("country") !== "BR") return;
+        NavigationHistory.saveForm();
+  
+        validateLocalized(country);
+  
+        
         const province = document.querySelector("[name=custrecord_lmry_addr_prov]");
         if (!province) return;
         const observer = new MutationObserver(async function (mutations) {
-            console.log(province.value);
             const selectCity = document.querySelector("#custpage_br_city");
-
+  
             selectCity.innerHTML = "<option value=''> </option>";
             if (Number(province.value)) {
-                const cities = search
-                    .create({
-                        type: "customrecord_lmry_city",
-                        filters: [["custrecord_lmry_prov_link", "anyof", province.value]],
-                        columns: [
-                            search.createColumn({
-                                name: "name",
-                                sort: search.Sort.ASC
-                            }),
-                            "internalid"
-                        ]
-                    })
-                    .run()
-                    .getRange(0, 1000);
-
+                const cities = getCities(province);
+  
                 cities.forEach((cityData) => {
                     selectCity.innerHTML += `<option value='${cityData.getValue("internalid")}'>${cityData.getValue("name")}</option>`;
                 });
@@ -50,15 +31,15 @@ define([
                 }
             }
         });
-
+  
         // Configurar las opciones del observador
         const config = { attributes: true, childList: false, subtree: false };
-
+  
         // Iniciar la observación del elemento input con las opciones configuradas
         observer.observe(province, config);
-
+  
         document.querySelector('[data-walkthrough="Field:custrecord_lmry_addr_city"]').style.display = "none";
-
+  
         document.querySelector('[data-walkthrough="Field:custrecord_lmry_addr_city"]').parentElement.innerHTML +=
             `<div class="uir-field-wrapper" data-nsps-label="Latam - City" data-nsps-type="field" data-field-type="select" data-walkthrough="Field:custrecord_lmry_addr_prov">
     <span id="custrecord_lmry_addr_city_fs_lbl_uir_label" class="smallgraytextnolink uir-label" data-nsps-type="field_label"
@@ -81,20 +62,49 @@ define([
         </select>
     </span>
     </div>`;
+        if (Number(recordNow.getValue("custrecord_lmry_addr_city"))) {
+            const selectCity = document.querySelector("#custpage_br_city");
+            selectCity.innerHTML = "<option value=''> </option>";
+            const cities = getCities(province);
+            cities.forEach((cityData) => {
+                selectCity.innerHTML += `<option value='${cityData.getValue("internalid")}'>${cityData.getValue("name")}</option>`;
+            });
+            document.querySelector("#custpage_br_city").value = recordNow.getValue("custrecord_lmry_addr_city");
+        }
         const selectCity = document.querySelector("#custpage_br_city");
         selectCity.addEventListener("change", (ev) => {
-            console.log(ev.target.value);
             recordNow.setValue("custrecord_lmry_addr_city", ev.target.value);
         });
     }
-
-    function validateLocalized(newRecord) {
+  
+    function getCities(province){
+        return search
+        .create({
+            type: "customrecord_lmry_city",
+            filters: [
+                ["custrecord_lmry_prov_link", "anyof", province.value],
+                "AND",
+                ["isinactive","is","F"]
+            ],
+            columns: [
+                search.createColumn({
+                    name: "name",
+                    sort: search.Sort.ASC
+                }),
+                "internalid"
+            ]
+        })
+        .run()
+        .getRange(0, 1000);
+    }
+  
+    function validateLocalized(country) {
         if (!runtime.isFeatureInEffect({ feature: 'SUBSIDIARIES' })) return false;
-    
-        const { href, searchParams } = new URL(document.referrer);
+        NavigationHistory.setURL(document.referrer)
+        const { href, searchParams } = new URL(NavigationHistory.findUrl());
         const subsidiaryID = searchParams.get(href.includes("subsidiarytype") ? "id" : "subsidiary");
     
-        if (subsidiaryID && !isLocalized(subsidiaryID)) {
+        if (subsidiaryID && !isLocalized(subsidiaryID,country)) {
             hideLatamFields();
         } else if (href.includes("entity")) {
             const entityID = searchParams.get("id");
@@ -104,49 +114,99 @@ define([
                 columns: ['subsidiary']
             }).subsidiary?.[0]?.value;
     
-            if (subsidiaryID && !isLocalized(subsidiaryID)) hideLatamFields();
+            if (subsidiaryID && !isLocalized(subsidiaryID,country)) hideLatamFields();
+        } else if (href.includes("transactions")){
+            const transactionID = searchParams.get("id");
+            const subsidiaryID = search.lookupFields({
+                type: search.Type.TRANSACTION,
+                id: transactionID,
+                columns: ['subsidiary']
+            }).subsidiary?.[0]?.value;
+            if (subsidiaryID && !isLocalized(subsidiaryID,country)) hideLatamFields();
         }
     }
-    
-    function isLocalized(subsidiaryID) {
-        let exist;
+  
+    function isLocalized(subsidiaryID,country) {
+
+        var featureInterCompany = runtime.getCurrentScript().getParameter({ name: "custscript_lmry_all_entity_fields" });
+
+        var filters = [
+            ['isinactive', 'is', 'F'],
+            "AND",
+            ['custrecord_lmry_features_subsidiary.internalid', 'anyof', subsidiaryID],
+            "AND",
+            ['custrecord_lmry_features_subsidiary.isinactive', 'is', 'F']
+        ];
+
+        if (!featureInterCompany) {
+            filters.push("AND",['custrecord_lmry_features_subsidiary.country', 'anyof', country]);
+        }
         const searchOW = search.create({
             type: 'customrecord_lmry_features_by_subsi',
             columns: [
-                'custrecord_lmry_features_subsidiary.isinactive'
+                'internalid'
             ],
-            filters: [
-                ['isinactive', 'is', 'F'],
-                "AND",
-                ['custrecord_lmry_features_subsidiary.internalid', 'anyof', subsidiaryID]
-            ]
+            filters: filters
         });
-        //const count = searchOW.runPaged().count;
-        //console.log("count :",count);
-        searchOW.run().each(result => {
-            const inactive = result.getValue(result.columns[0]);
-            exist = inactive !== "T" && inactive !== true;
-        });
-        console.log("isLocalized",exist);   
-        return exist;
-
+        return searchOW.runPaged().count;
     }
-
     function hideLatamFields(){
-        console.log("Ocultando Campos...")
-
         const rowstable = document.querySelectorAll("tr");
-
         rowstable.forEach(row => {
             const div = row.querySelector("td>div");
             if (div && div.getAttribute("data-walkthrough")?.startsWith("Field:custrecord_lmry")) {
-                console.log(div.getAttribute("data-walkthrough"))
                 row.style.display = "none";
             }
         });
-        console.log("Se ha ocultado los campos latam")
     }
+  
+  
+    class NavigationHistory {
+        // Método para agregar una URL al historial
+        static setURL(urlUnit) {
+            let history = JSON.parse(sessionStorage.getItem('navigationHistory')) || [];
+            history.push(urlUnit);
+            sessionStorage.setItem('navigationHistory', JSON.stringify(history));
+        }
+    
+        // Método para obtener el historial completo
+        static getURLS() {
+            return JSON.parse(sessionStorage.getItem('navigationHistory')) || [];
+        }
+    
+        // Método para eliminar el historial completo
+        static deleteURLS() { 
+            sessionStorage.removeItem('navigationHistory');
+        }
+  
+        static saveForm(){
+            const okButton = document.querySelector('input[name="ok"]');
+            if (okButton) {
+                okButton.addEventListener('mousedown', function () {
+                    NavigationHistory.deleteURLS();
+                });
+            }
+  
+            const okClose = document.querySelector('input[name="close"]');
+            if (okClose) {
+                okClose.addEventListener('mousedown', function () {
+                    NavigationHistory.deleteURLS();
+                });
+            }
+        }
+    
+        // Método para buscar la última URL con los parámetros específicos
+        static findUrl() {
+            const urls = NavigationHistory.getURLS();
+            return [...urls].reverse().find(urlUnit => 
+                urlUnit.includes("subsidiarytype") || urlUnit.includes("subsidiary") || urlUnit.includes("entity") || urlUnit.includes("transactions")
+            );
+        }
+    }
+  
+  
     return {
         brCustomCity
     };
-});
+  });
+  
